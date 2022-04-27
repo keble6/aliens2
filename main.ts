1fbeef4a02ff497fcaa723c7ecf46a2d6a9c8c74
Rob Wilson <rob@rwilson.org.uk>
2022-04-27 10:42:14 +0100

    improved B function

--- main.ts
+++ main.ts
@@ -7,22 +7,33 @@ input.onButtonPressed(Button.A, function () {
 input.onButtonPressed(Button.B, function () {
     // missile moves from defender x position
     missile = game.createSprite(defender.get(LedSpriteProperty.X), 4)
-    missileActive = true
     missile.set(LedSpriteProperty.Blink, 1)
     // set direction to up
     missile.set(LedSpriteProperty.Direction, 0)
+    basic.pause(missileDelay)
+    // Launch the missile
+    for (let index = 0; index < 4; index++) {
+        missile.move(1)
+        basic.pause(missileDelay)
+    }
+    // Has the missile hit the alien?
+    if (missile.isTouching(alien)) {
+        basic.showIcon(IconNames.Butterfly)
+    }
+    missile.delete()
 })
 let bomb: game.LedSprite = null
-let missileActive = false
 let missile: game.LedSprite = null
+let missileDelay = 0
 let defender: game.LedSprite = null
+let alien: game.LedSprite = null
 // alien moves across the top of screen
-let alien = game.createSprite(0, 0)
+alien = game.createSprite(0, 0)
 let alienDelay = 1000
 let bombDelay = 500
 // defender moves across the bottom of screen
 defender = game.createSprite(0, 4)
-let missileDelay = 200
+missileDelay = 200
 game.setScore(0)
 basic.forever(function () {
     basic.pause(alienDelay)
@@ -42,21 +53,5 @@ basic.forever(function () {
         }
         bomb.delete()
         alien.set(LedSpriteProperty.Blink, 0)
-    }
-    // Is the missile active?
-    if (missileActive) {
-        basic.pause(missileDelay)
-        // Launch the missile
-        for (let index = 0; index < 4; index++) {
-            missile.move(1)
-            basic.pause(missileDelay)
-        }
-        // Has the missile hit the alien?
-        if (missile.isTouching(alien)) {
-            basic.showIcon(IconNames.Butterfly)
-            missile.delete()
-        }
-        missileActive = false
-        missile.delete()
     }
 })
